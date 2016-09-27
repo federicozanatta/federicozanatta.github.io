3eba7aa4b210da0b76ad016566c72665b2eec075
nicolas sigal <nicolas.sigal@globallogic.com>
2016-09-27 09:02:53 -0300

    initial

--- gulp-tasks/bundle.js
+++ gulp-tasks/bundle.js
@@ -11,7 +11,7 @@ function bundle(gulp, $){
 	var buffer = require('vinyl-buffer');
 	$.paths.bundle = {
 		resources: [
-		// './bower_components/bootstrap/dist/js/bootstrap.min.js',
+		'./node_modules/jquery/dist/jquery.min.js',
 		'./node_modules/angular/angular.min.js',
 		'./node_modules/angular-ui-router/release/angular-ui-router.min.js',
 		'./node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
