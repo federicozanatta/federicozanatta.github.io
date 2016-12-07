69658fe28fd929f7ab93d7dcdf086917d4b144b2
Federico Zanatta <federico.zanatta@synapse.com>
2016-12-07 15:10:23 -0300

    new commit1

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -153,11 +153,15 @@ function pre_ingresoController(angular, app) {
 
     pre_ingresoCtrl.$inject = ['$timeout', '$mdSidenav','$state'];
 
-    function pre_ingresoCtrl($timeout, $mdSidenav,$state){
+    function pre_ingresoCtrl($timeout, $mdSidenav, $state){
         var self = this; //jshint ignore:line
         function send(){
             $state.go('cirugia.intervencion',{ user: self.user });
         }
+        function myFunction() {
+        var x = document.getElementById("pre_ingreso.user.circulante").value;
+        document.getElementById("demo").innerHTML = x;
+}
         function volverCirugia(){
             $state.go('cirugia',{ user: self.user });
         }
@@ -176,13 +180,17 @@ function pre_ingresoController(angular, app) {
             self.toggleRight = toggleRight;
             self.send = send;
             self.volverCirugia = volverCirugia;
+            self.myFunction = myFunction;
         }
         init();
+            var variable = new Date(); 
+var coso = (variable.getDate() + "/" + (variable.getMonth() +1) + "/" + variable.getFullYear());
+document.getElementById("pruebafecha").innerHTML = coso;
     }
 }
-//var variable = new Date(); 
-//var coso = (variable.getDate() + "/" + (variable.getMonth() +1) + "/" + variable.getFullYear());
+//document.getElementsByName("pre_ingreso.user.date")[0].value = coso;
 //document.write("dia" + coso);
+//document.getElementsByName("pre_ingreso.user.date")[0].innerHTML = coso;
 module.exports = pre_ingresoController
 
 },{}],6:[function(require,module,exports){
